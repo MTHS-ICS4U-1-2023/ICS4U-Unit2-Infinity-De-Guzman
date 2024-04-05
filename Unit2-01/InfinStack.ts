7d8e1fbdbca9794c3956551348c862f9479d9432
Infinity de Guzman <infinity.deguzman@mths.ca>
2024-04-05 10:06:27 -0400

    bruh

---
+++ Unit2-01/InfinStack.ts
@@ -0,0 +1,29 @@
+/**
+ * author   Infinity de Guzman
+ * version  1.0
+ * since    2024-04-03
+ */
+class InfinStack {
+    // variables
+    constructor() {
+        self.stackAsList = []
+    }
+
+    // This function pushes the object to a list
+    push(input) {
+        self.stackAsList.push(input)
+    }
+
+    // This function prints out the object
+    showStack() {
+        let values = ""
+        for (let counter = 0; counter < self.stackAsList.length; counter++) {
+            values = values + self.stackAsList[counter] + ", "
+        }
+        values = values.substring(0, values.length-2)
+        return values
+    }
+}
+
+export default InfinStack
+
