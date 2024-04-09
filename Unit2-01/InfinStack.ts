e86a4ad5b132345f6711980a90f3c57882103192
Infinity de Guzman <infinity.deguzman@mths.ca>
2024-04-09 10:00:33 -0400

    :D

--- Unit2-01/InfinStack.ts
+++ Unit2-01/InfinStack.ts
@@ -4,24 +4,24 @@
  * since    2024-04-03
  */
 export class InfinStack {
-    // variables
-    constructor() {
-        self.stackAsList = []
-    }
+  // variables
+  constructor() {
+    self.stackAsList = []
+  }
 
-    // This function pushes the object to a list
-    push(input) {
-        self.stackAsList.push(input)
-    }
+  // This function pushes the object to a list
+  push(input) {
+    self.stackAsList.push(input)
+  }
 
-    // This function prints out the object
-    showStack() {
-        let stackValues = ""
-        for (let counter = 0; counter < self.stackAsList.length; counter++) {
-            stackValues = stackValues + self.stackAsList[counter] + ", "
-        }
-        stackValues = stackValues.substring(0, stackValues.length-2)
-        return stackValues
+  // This function prints out the object
+  showStack() {
+    let stackValues = ""
+    for (let counter = 0; counter < self.stackAsList.length; counter++) {
+      stackValues = stackValues + self.stackAsList[counter] + ", "
     }
+    stackValues = stackValues.substring(0, stackValues.length-2)
+    return stackValues
+  }
 }
 
--- Unit2-01/java/InfinStack.java
+++ Unit2-01/java/InfinStack.java
@@ -5,4 +5,41 @@
  * @version   1.0
  * @since     2024-04-09
  */
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is the class InfinStack.
+ */
+public class CurtisStack {
+    /**
+     * Declares that the instance variable is private.
+     */
+        private List<String> stackAsList = new ArrayList<>();
+
+    /**
+     * The push method.
+     *
+     * @param input from Main.java
+     */
+    public void push(String input) {
+        stackAsList.add(input);
+    }
+
+    /**
+     * showStack.
+     *
+     * @return the stackValues as a string
+     */
+    public String showStack() {
+        String stackValues = "";
+        for (int counter = 0; counter < stackAsList.size(); counter++) {
+            stackValues += stackAsList.get(counter);
+            if (counter < stackAsList.size() - 1) {
+                stackValues += ", ";
+            }
+        }
+    return values;
+    }
+}
 
