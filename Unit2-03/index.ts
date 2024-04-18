888f6214a5ea5f04347e3d987cfa4f198ed8644c
Infinity de Guzman <infinity.deguzman@mths.ca>
2024-04-18 10:04:58 -0400

    do 2-03 now

---
+++ Unit2-03/index.ts
@@ -0,0 +1,42 @@
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author   Infinity de Guzman
+ * version  1.0
+ * since    2024-04-03
+ */
+import { InfinStack } from './InfinStack'
+
+let jewels = new InfinStack()
+jewels.push("garnet")
+jewels.push("amethyst")
+jewels.push("sapphire")
+console.log("jewels: " + jewels.showStack)
+console.log("popped: " + jewels.itemPop())
+console.log("jewels: " + jewels.showStack)
+console.log("")
+
+let pokemon = new InfinStack()
+pokemon.push("oshawott")
+pokemon.push("pichu")
+pokemon.push("wobbuffet")
+console.log("pokemon: " + pokemon.showStack)
+console.log("popped: " + pokemon.itemPop())
+console.log("popped: " + pokemon.itemPop())
+console.log("pokemon: " + pokemon.showStack)
+console.log("")
+
+let colours = new InfinStack()
+colours.push("aquamarine")
+colours.push("indigo")
+colours.push("teal")
+console.log("colours: " + colours.showStack)
+console.log("popped: " + colours.itemPop())
+console.log("popped: " + colours.itemPop())
+console.log("popped: " + colours.itemPop())
+console.log("popped: " + colours.itemPop())
+console.log("colours: " + colours.showStack)
+console.log("")
+
+console.log("\nDone.")
+
