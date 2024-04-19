8467ef133987ce195cbbdfa0dd3bde77bd75509c
Infinity :0 <82898183+Infinity-deGuzman@users.noreply.github.com>
2024-04-19 15:57:01 +0000

    .

---
+++ Unit2-04/index.ts
@@ -0,0 +1,57 @@
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author   Infinity de Guzman
+ * version  1.0
+ * since    2024-04-03
+ */
+import { InfinStack } from './InfinStack'
+
+let jewels = new InfinStack()
+jewels.push("garnet")
+jewels.push("amethyst")
+jewels.push("sapphire")
+console.log("jewels: " + jewels.showStack)
+console.log("peak: " + jewels.peak)
+console.log("popped: " + jewels.itemPop())
+console.log("jewels: " + jewels.showStack)
+fruits.clear()
+console.log("Clearing stack...")
+console.log("jewels: " + jewels.showStack)
+console.log("")
+
+let pokemon = new InfinStack()
+pokemon.push("oshawott")
+pokemon.push("pichu")
+pokemon.push("wobbuffet")
+console.log("pokemon: " + pokemon.showStack)
+console.log("popped: " + pokemon.itemPop())
+console.log("popped: " + pokemon.itemPop())
+console.log("pokemon: " + pokemon.showStack)
+console.log("peak: " + pokemon.peak)
+console.log("pokemon: " + pokemon.showStack)
+pokemon.clear()
+console.log("Clearing stack...")
+console.log("pokemon: " + pokemon.showStack)
+console.log("")
+
+
+let colours = new InfinStack()
+colours.push("aquamarine")
+colours.push("indigo")
+colours.push("teal")
+console.log("colours: " + colours.showStack)
+console.log("popped: " + colours.itemPop())
+console.log("popped: " + colours.itemPop())
+console.log("popped: " + colours.itemPop())
+console.log("popped: " + colours.itemPop())
+console.log("colours: " + colours.showStack)
+console.log("peak: " + colours.peak)
+console.log("colours: " + colours.showStack)
+colours.clear()
+console.log("Clearing stack...")
+console.log("colours: " + colours.showStack)
+console.log("")
+
+console.log("\nDone.")
+
